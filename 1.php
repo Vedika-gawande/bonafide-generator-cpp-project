1fe0be3b67287b4b7c4db9858d02400c93a2cfbb
Vedika Gawande <vedikagawande91@gmail.com>
2026-03-23 08:38:59 +0530

    Delete 1.php

--- 1.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="2.php">
-    <label for="enrollment_number">Enter Enrollment Number:</label>
-    <input type="text" name="enrollment_number" required>
-    <button type="submit" name="search">Search</button>
-</form>
